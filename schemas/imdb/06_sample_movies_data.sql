09e153975bd92860e99bef7cf4ae0a95657c09a6
Satish Shrestha <pansupari4u@gmail.com>
2026-06-04 14:27:04 +0200

    Add IMDB schema files

---
+++ schemas/imdb/06_sample_movies_data.sql
@@ -0,0 +1,14 @@
+-- Sample data for IMDB table: movies
+-- Only 10 sample rows are included. The original dump contains much more data.
+
+INSERT INTO `movies` VALUES
+(0,0,'#28',2002,NULL),
+(1,1,'#7 Train: An Immigrant Journey, The',2000,NULL),
+(2,2,'$',1971,6.4),
+(3,3,'$1,000 Reward',1913,NULL),
+(4,4,'$1,000 Reward',1915,NULL),
+(5,5,'$1,000 Reward',1923,NULL),
+(6,6,'$1,000,000 Duck',1971,5),
+(7,7,'$1,000,000 Reward, The',1920,NULL),
+(8,8,'$10,000 Under a Pillow',1921,NULL),
+(9,9,'$100,000',1915,NULL);
